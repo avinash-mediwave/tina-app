e9b5452bc894bbcfdf7d8553a28ed36a147e222c
avinash-mediwave <avinash@mindwaveventures.com>
2023-02-07 18:25:59 +0530

    tina config changes

--- .tina/.tina/config.ts
+++ .tina/.tina/config.ts
@@ -5,8 +5,8 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "master"
 
 export default defineConfig({
   branch,
-  clientId: 'fa70ac85-c21d-42e4-8c59-3a2a09df814d', // Get this from tina.io
-  token: 'ae355e434dc5b5a753201ef9e95f228345c2daa4', // Get this from tina.io
+  clientId: "fa70ac85-c21d-42e4-8c59-3a2a09df814d", // Get this from tina.io
+  token: "ae355e434dc5b5a753201ef9e95f228345c2daa4", // Get this from tina.io
   build: {
     outputFolder: "admin",
     publicFolder: "public",
