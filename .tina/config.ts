c6c18e759d4345ee119456d8e4f056bcf4f4daec
avinash-mediwave <avinash@mindwaveventures.com>
2023-02-07 12:02:43 +0530

    tina config changes

---
+++ .tina/config.ts
@@ -0,0 +1,44 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "master";
+
+export default defineConfig({
+  branch,
+  clientId: 'fa70ac85-c21d-42e4-8c59-3a2a09df814d', // Get this from tina.io
+  token: 'ae355e434dc5b5a753201ef9e95f228345c2daa4', // Get this from tina.io
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "public",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
